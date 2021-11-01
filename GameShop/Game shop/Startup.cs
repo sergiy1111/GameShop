7fc7bdf254a6845f4f6111447b05a73bdb0d1f83
sergiy1111 <grigha71@gmail.com>
2021-11-01 15:00:46 +0200

    Add program

---
+++ GameShop/Game shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Game_shop.Startup))]
+namespace Game_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
